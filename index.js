a1502a72bcb586d6dd4ee7c3c2a3422afe4119c0
Valerij Kalabin <valerijkalabin@yandex.ru>
2020-09-10 17:31:22 +0700

    create modal element and parallax

--- index.js
+++ index.js
@@ -1,24 +1,45 @@
 const arrowDownList = Array.from(document.querySelectorAll('.fa-arrow-alt-circle-down'));
 const arrowUpList = Array.from(document.querySelectorAll('.fa-arrow-alt-circle-up'));
+const form = document.querySelector('#form');
+const btnForm = form.querySelector('.btn-form');
 
-const openDescription = function(arrowDownItem) {
+const modalBody = document.querySelector('.modal-body');
+const modalSurname = modalBody.querySelector('.modal-surname');
+const modalName = modalBody.querySelector('.modal-name');
+const modalPhone = modalBody.querySelector('.modal-phone');
+const modalEmail = modalBody.querySelector('.modal-email');
+
+const openCardDescription = function(arrowDownItem) {
     arrowDownItem.classList.add('d-none');
     arrowDownItem.nextElementSibling.classList.remove('d-none');
     arrowDownItem.parentElement.nextElementSibling.classList.remove('d-none');
 };
-const closeDescription = function(arrowUpItem) {
+const closeCardDescription = function(arrowUpItem) {
     arrowUpItem.classList.add('d-none');
     arrowUpItem.previousElementSibling.classList.remove('d-none');
     arrowUpItem.parentElement.nextElementSibling.classList.add('d-none');
 };
 
+const setModalBody = function() {
+    modalSurname.textContent = form.elements.exampleInputSurname.value;
+    modalName.textContent = form.elements.exampleInputName.value;
+    modalPhone.textContent = form.elements.exampleInputPhone.value;
+    modalEmail.textContent = form.elements.exampleInputEmail1.value;
+};
+
 arrowDownList.forEach((arrowDownItem) => {
     arrowDownItem.addEventListener('click', () => {
-        openDescription(arrowDownItem)
+        openCardDescription(arrowDownItem)
     });
 });
 arrowUpList.forEach((arrowUpItem) => {
     arrowUpItem.addEventListener('click', () => {
-        closeDescription(arrowUpItem)
+        closeCardDescription(arrowUpItem)
     });
 });
+
+form.addEventListener('submit', (evt) => {
+    evt.preventDefault();
+});
+
+btnForm.addEventListener('click', setModalBody);
